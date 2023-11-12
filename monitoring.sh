47d713bd374cedd690f7a4393c4e5e2e392f6c4b
maroth <maroth@student.42lausanne.ch>
2023-11-12 20:45:42 +0100

    Script should work, need to test on VM

--- monitoring.sh
+++ monitoring.sh
@@ -10,12 +10,27 @@ total_disk=$(df -h | grep /$ | awk '{print $2}')
 per_disk=$(df -h | grep /$ | awk '{print $5}')
 cpu_load=$(top -bn1 | grep ^top | awk '{print $(NF-2)}' | tr -d ',')
 last_boot=$(who -b | awk '{print $(NF-1)" "$NF}')
+count_lvm=$(lsblk | grep lvm | wc -l)
+if [count_lvm -gt 0]; then
+	lvm=$(echo Yes)
+else
+	lvm=$(echo No)
+fi
+active_connections=$(ss -s | grep estab | awk '{print $4}' | tr -d ',')
+count_users=$(users | wc -w)
+ipv4=$(hostname -I)
+mac=$(ip link show | grep ether | awk '{print $2}')
+count_sudo=$(grep COMMAND /var/log/sudo/sudo.log | wc -l)
 
-wall "	#Architecture:	$arch
+echo "	#Architecture:	$arch
 	#CPU physical:	$cpu
 	#vCPU:	$vcpu
 	#Memory Usage:	${used_ram}/${total_ram}MiB (${per_ram}%)
 	#Disk Usage:	${used_disk}/${total_disk} ($per_disk)
 	#CPU load:	${cpu_load}%
 	#Last boot:	$last_boot
-	#LVM use:	$lvm"
+	#LVM use:	$lvm
+	#Connections TCP:	$active_connections ESTABLISHED
+	#User log:	$count_users
+	#Network:	IP $ipv4($mac)
+	#Sudo:	$count_sudo cmd"
